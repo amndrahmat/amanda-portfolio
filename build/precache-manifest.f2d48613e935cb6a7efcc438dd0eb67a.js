cde35ca4a471dd501a93627a5d6a148b4162bc9b
Amanda Rahmat Hidayat <amanda.rahmat@ai.astra.co.id>
2024-04-02 12:24:01 +0700

    add terbaru

---
+++ build/precache-manifest.f2d48613e935cb6a7efcc438dd0eb67a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9db727e1a519026c7f14d6fa5caa02c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4063faa28acbeb9ac18a",
+    "url": "/static/css/main.47d7a882.chunk.css"
+  },
+  {
+    "revision": "7bc9834477d3650f2de6",
+    "url": "/static/js/2.c30503c6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.c30503c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4063faa28acbeb9ac18a",
+    "url": "/static/js/main.00d1e25d.chunk.js"
+  },
+  {
+    "revision": "3fa886ab9a76ed7605cb",
+    "url": "/static/js/runtime-main.cfcf1c5b.js"
+  }
+]);
